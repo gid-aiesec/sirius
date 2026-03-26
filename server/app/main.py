3143f30145aac7f6356224e60ac00da5611bf547
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-03-26 04:33:54 +0400

    feat: added cors handling

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.health import router as health_router
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    )
 app.include_router(health_router)
