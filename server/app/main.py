950e2e80b9df6efb90d761099040c67bf2de56c8
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-03-26 14:34:52 +0400

    fix: changes the allowed origins and all the cors information now gets pulled from the env file

--- server/app/main.py
+++ server/app/main.py
@@ -1,13 +1,18 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from app.routes.health import router as health_router
+
+load_dotenv()
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
+    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
+    allow_methods=[os.getenv("ALLOWED_METHODS")],
+    allow_headers=[os.getenv("ALLOWED_HEADERS")],
+    allow_credentials=os.getenv("ALLOWED_CREDENTIALS"),
     )
 app.include_router(health_router)
